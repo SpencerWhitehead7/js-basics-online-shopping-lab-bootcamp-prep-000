2ec611dfa01928fe77e37d05b96410a6e4cfd72a
Spencer Whitehead <spencerwhitehead7@gmail.com>
2018-01-20 01:39:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -72,4 +72,8 @@ function placeOrder(cardNumber) {
   if(typeof cardNumber === "undefined"){
     console.log("Sorry, we don't have a credit card on file for you.");
   }
+  else {
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
+    var cart = []
+  }
 }
