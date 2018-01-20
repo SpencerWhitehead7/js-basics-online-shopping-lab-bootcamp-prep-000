babc7add3407a49ae9a734c2e11cd41c395d8cde
Spencer Whitehead <spencerwhitehead7@gmail.com>
2018-01-20 01:36:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -73,8 +73,6 @@ function placeOrder(cardNumber) {
     console.log("Sorry, we don't have a credit card on file for you.");
   }
   else {
-    const cartTotal = total();
-    console.log(`Your total cost is $${cartTotal}, which will  be charged to the card ${cardNumber}.`);
-    var cart = [];
+    
   }
 }
